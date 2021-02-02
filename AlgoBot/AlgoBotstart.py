b3c501ff74a5e5641042ac3cee1c976536457a70
Samuel Recio <sam.recio11@gmail.com>
2021-02-02 08:10:56 +0100

    all

--- AlgoBot/AlgoBotstart.py
+++ AlgoBot/AlgoBotstart.py
@@ -7,6 +7,20 @@ from IPython.display import display
 import certifi
 import urllib3
 import requests
+import csv
+import matplotlib.pyplot as plt
+import qgrid
+import datetime as dt
+import btalib
+
+data = pd.read_csv("BTCUSDT-1m-2018-03.csv")
+print(data.head())
+
+btc_df['20MA'] = btalib.sma(btc_df.close, period=20).df
+print(btc_df.tail())
+
+rsi = btalib.rsi(btc_df, period=11)
+print(rsi.tail())
 
 api_key = os.environ.get('binance_api')
 api_secret = os.environ.get('binance_secret')
@@ -18,17 +32,10 @@ client.API_URL = 'https://testnet.binance.vision/api'
 
 test = requests.get('https://testnet.binance.vision/api')
 cafile = certifi.where()
-#with open('binance-vision.pem', 'rb')
-#as infile:
-    #customca = infile.read()
-#with open(cafile, 'ab') as outfile:
-    #outfile.write(customca)
 
 
-print(client.get_account())
 
 def btc_trade_history(msg):
-    ''' define how to process incoming WebSocket messages '''
     if msg['e'] != 'error':
         print(msg['c'])
         btc_price['last'] = msg['c']
@@ -36,4 +43,36 @@ def btc_trade_history(msg):
         btc_price['last'] = msg['a']
     else:
         btc_price['error'] = True
+
+
+
+bsm = BinanceSocketManager(client)
+conn_key = bsm.start_symbol_ticker_socket('BTCUSDT', btc_trade_history)
+#bsm.start()
+
+#print(btc_price)
+
+
+
+
+
+#btc_price['MA'] = btc_price['last'].rolling().mean()
+
+#btc_price['Upper'] = btc_price['MA'] + (2*btc_price['last'].rolling().std())
+
+#btc_price['Lower'] = btc_price['MA'] - (2*btc_price['last'].rolling().std())
+
+#btc_price[['last','MA','Upper','Lower']].plot(figsize=(16,6))
+
+
+
+
+
+
+
+#print(client.get_account())
+
+
+
+
 
--- AlgoBot/BollingerBands.py
+++ AlgoBot/BollingerBands.py
@@ -1,35 +1,24 @@
+import os
 import pandas as pd
-from pandas_datareader import data, wb
-import datetime#Define start and end date
-start = pd.to_datetime('2018-02-04')
-end = pd.to_datetime('2020-05-29')#Import market data
-jacobs_df = data.DataReader('J', 'yahoo', start , end)
-jacobs_df#Appliying Bollinger Band strategy
-jacobs_df['Middle Band'] = jacobs_df['Close'].rolling(window=20).mean()jacobs_df['Upper Band'] = jacobs_df['Middle Band'] + 1.96*jacobs_df['Close'].rolling(window=20).std()jacobs_df['Lower Band'] = jacobs_df['Middle Band'] - 1.96*jacobs_df['Close'].rolling(window=20).std()#Checking the dataframe
-jacobs_df#Visualise data using PlotLy
-import plotly.graph_objs as go
-#declare figure
-fig = go.Figure()#Set up traces
-fig.add_trace(go.Scatter(x=jacobs_df.index, y= jacobs_df['Middle Band'],line=dict(color='blue', width=.7), name = 'Middle Band'))
-fig.add_trace(go.Scatter(x=jacobs_df.index, y= jacobs_df['Upper Band'],line=dict(color='red', width=1.5), name = 'Upper Band (Sell)'))
-fig.add_trace(go.Scatter(x=jacobs_df.index, y= jacobs_df['Lower Band'],line=dict(color='green', width=1.5), name = 'Lower Band (Buy)'))fig.add_trace(go.Candlestick(x=jacobs_df.index,
-                open=jacobs_df['Open'],
-                high=jacobs_df['High'],
-                low=jacobs_df['Low'],
-                close=jacobs_df['Close'], name = 'market data'))# Add titles
-fig.update_layout(
-    title='Bollinger Band Strategy',
-    yaxis_title='Jacobs Engineering Stock Price (USD per Shares)')# X-Axes
-fig.update_xaxes(
-    rangeslider_visible=True,
-    rangeselector=dict(
-        buttons=list([
-            dict(count=1, label="1m", step="month", stepmode="backward"),
-            dict(count=6, label="6m", step="month", stepmode="backward"),
-            dict(count=1, label="YTD", step="year", stepmode="todate"),
-            dict(count=1, label="1y", step="year", stepmode="backward"),
-            dict(step="all")
-        ])
-    )
-)#Show
-fig.show()
+from binance.client import Client
+from binance.websockets import BinanceSocketManager
+from twisted.internet import reactor
+from IPython.display import display
+import certifi
+import urllib3
+import requests
+import time
+import csv
+import matplotlib.pyplot as plt
+import qgrid
+import datetime as dt
+
+btc = client.get_symbol_ticker(symbol="BTCUSDT")
+
+btc['20 day MA'] = btc['Last'].rolling(20).mean()
+
+btc['Upper'] = btc['20 day MA'] + (2*btc['Last'].rolling(20).std())
+
+btc['Lower'] = btc['20 day MA'] - (2*btc['Last'].rolling(20).std())
+
+btc[['Last','20 day MA','Upper','Lower']].plot(figsize=(16,6))
